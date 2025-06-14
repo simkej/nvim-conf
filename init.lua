0c9e0ac39da38643fe175d0789d5054b666ca02a
Simon Kejonen <simon.m.kejonen97@gmail.com>
2025-06-15 00:55:24 +0200

    Better syntax

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.diagnostic.config({ virtual_text = true })
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.number = true
+vim.diagnostic.config({ virtual_text = true })
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
