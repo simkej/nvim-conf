861219817a40301a76e7ff152b5c3335b489ca52
Simon Kejonen <simon.m.kejonen97@gmail.com>
2025-08-09 00:27:43 +0200

    indent

--- init.lua
+++ init.lua
@@ -8,17 +8,17 @@ vim.diagnostic.config({ virtual_text = true })
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -27,69 +27,69 @@ vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
-    -- add your plugins here
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    {
-      'nvim-telescope/telescope.nvim', tag = '0.1.8',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"},
-    {
-      "nvim-neo-tree/neo-tree.nvim",
-      branch = "v3.x",
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons",
-        "MunifTanjim/nui.nvim",
-      },
-      lazy = false,
-      opts = {
-        source_selector = {
-          winbar = true
-        }
-      },
-    },
-    {
-      'nvim-lualine/lualine.nvim',
-      dependencies = { 'nvim-tree/nvim-web-devicons' }
-    },
-    {
-      "mason-org/mason.nvim",
-      opts = {}
-    },
-    {
-      "mason-org/mason-lspconfig.nvim",
-      opts = {},
-      dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-        "neovim/nvim-lspconfig",
-      },
-    },
-    {
-      "nvim-telescope/telescope-ui-select.nvim"
-    },
-    {
-      'saghen/blink.cmp',
-      dependencies = { 'rafamadriz/friendly-snippets' },
-      version = '1.*',
-      opts = {
-        keymap = { preset = 'default' },
-        appearance = {
-          nerd_font_variant = 'mono'
+    spec = {
+        -- add your plugins here
+        { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+        {
+            'nvim-telescope/telescope.nvim', tag = '0.1.8',
+            dependencies = { 'nvim-lua/plenary.nvim' }
         },
-      },
+        {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"},
+        {
+            "nvim-neo-tree/neo-tree.nvim",
+            branch = "v3.x",
+            dependencies = {
+                "nvim-lua/plenary.nvim",
+                "nvim-tree/nvim-web-devicons",
+                "MunifTanjim/nui.nvim",
+            },
+            lazy = false,
+            opts = {
+                source_selector = {
+                    winbar = true
+                }
+            },
+        },
+        {
+            'nvim-lualine/lualine.nvim',
+            dependencies = { 'nvim-tree/nvim-web-devicons' }
+        },
+        {
+            "mason-org/mason.nvim",
+            opts = {}
+        },
+        {
+            "mason-org/mason-lspconfig.nvim",
+            opts = {},
+            dependencies = {
+                { "mason-org/mason.nvim", opts = {} },
+                "neovim/nvim-lspconfig",
+            },
+        },
+        {
+            "nvim-telescope/telescope-ui-select.nvim"
+        },
+        {
+            'saghen/blink.cmp',
+            dependencies = { 'rafamadriz/friendly-snippets' },
+            version = '1.*',
+            opts = {
+                keymap = { preset = 'default' },
+                appearance = {
+                    nerd_font_variant = 'mono'
+                },
+            },
+        },
+        {
+            'windwp/nvim-autopairs',
+            event = "InsertEnter",
+            config = true
+        },
     },
-    {
-      'windwp/nvim-autopairs',
-      event = "InsertEnter",
-      config = true
-    },
-  },
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+    -- colorscheme that will be used when installing plugins.
+    install = { colorscheme = { "habamax" } },
+    -- automatically check for plugin updates
+    checker = { enabled = true },
 })
 
 vim.cmd.colorscheme "catppuccin"
@@ -101,19 +101,19 @@ vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' 
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
-  highlight = { enable = true},
-  indent = { enable = true},
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+    highlight = { enable = true},
+    indent = { enable = true},
 }
 
 vim.keymap.set('n', '<leader>e', ':Neotree<CR>')
 
 require('lualine').setup {
-  options = {theme = 'dracula'}
+    options = {theme = 'dracula'}
 }
 
 require('mason-lspconfig').setup {
-  ensure_installed = {"lua_ls", "clangd"}
+    ensure_installed = {"lua_ls", "clangd"}
 }
 
 vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
@@ -123,15 +123,15 @@ vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
 vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
 
 require("telescope").setup {
-  extensions = {
-    ["ui-select"] = {
-      require("telescope.themes").get_dropdown {
-      }
+    extensions = {
+        ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
+            }
+        }
     }
-  }
 }
 require("telescope").load_extension("ui-select")
 
 require("nvim-autopairs").setup({
-  check_ts = true,
+    check_ts = true,
 })
