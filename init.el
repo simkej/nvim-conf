fd986f20fb7fd33b645faa3ff305eaaf8b3e2954
Simon Kejonen <simon.m.kejonen97@gmail.com>
2025-08-25 13:34:52 +0200

    Changed emacs font

--- init.el
+++ init.el
@@ -65,5 +65,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "CommitMono Nerd Font" :foundry "    " :slant normal :weight regular :height 120 :width normal))))
+ '(default ((t (:family "JetBrainsMono Nerd Font" :foundry "JB" :slant normal :weight regular :height 120 :width normal))))
  '(company-template-field ((t (:background "dim gray" :foreground "white")))))
